d0e43a4ce9c1f66ea73ae9fbd6c04b4fc6361880
ADYSTECH <ADYSTECH@ADYSTECH-PC>
2015-04-28 12:43:14 +0700

    perkalian (12621052)

---
+++ src/perkalian052/Perkalian052.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package perkalian052;
+
+public class Perkalian052 {
+
+    public static void main(String[] args) {
+    int x = 12;
+int y = 4;
+int s = x*y;
+System.out.println("nilai= " +x);
+System.out.println("nilai= " +y);
+System.out.println("nilai= " +s);
+    }
+}
